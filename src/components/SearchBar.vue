77cedb7406682f53ad35b9a26a895b0003bd208e
simotae14 <startae14@gmail.com>
2019-03-26 01:02:19 +0100

    emit event in SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -9,6 +9,8 @@ export default {
     name: 'SearchBar',
     methods: {
         onInput: function(event) {
+            // emit event
+            this.$emit('termChange', event.target.value);
             console.log(event.target.value);
         }
     }
